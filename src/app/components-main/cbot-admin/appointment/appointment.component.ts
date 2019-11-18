b5141bb2352e668ca0e846d553252d36f77dd7a2
sivagami-cbotsolutions <sivagami@cbotsolutions.com>
2019-11-18 17:06:01 +0530

    calendar month view list view

--- src/app/components-main/cbot-admin/appointment/appointment.component.ts
+++ src/app/components-main/cbot-admin/appointment/appointment.component.ts
@@ -7,6 +7,7 @@ import Swal from 'sweetalert2';
 import { FullCalendarComponent } from '@fullcalendar/angular';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGrigPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
 import interactionPlugin, { Draggable } from '@fullcalendar/interaction';
 import resourceTimeGridPlugin from '@fullcalendar/resource-timegrid';
 import { Router } from '@angular/router';
@@ -40,6 +41,15 @@ const ELEMENT_DATA: PeriodicElement[] = [
   {sno: 12, name: 'Nithya', mobile: '9678125432',serviceName:'Threading',staffName:'Nisha',startTime:'12:45',serviceDuration:'00:15',servicePrice:'50.00',status:'booked',source:'Phone'},
   {sno: 13, name: 'Anbu', mobile: '7684957545',serviceName:'Hair Cut',staffName:'Sasi',startTime:'09:50',serviceDuration:'00:20',servicePrice:'120.00',status:'Arrived',source:'Phone'},
   {sno: 14, name: 'Sasi', mobile: '9283714565',serviceName:'Facial',staffName:'Prakash',startTime:'09:20',serviceDuration:'01:00',servicePrice:'700.00',status:'Arrived',source:'Phone'},
+];
+
+export interface Service {
+  name: string
+}
+
+const SERVICE_DATA: Service[] = [
+  {name: 'HaiCut'},
+  {name: 'Bleech'}
 ];
 export interface Status {
   value: string;
@@ -78,6 +88,7 @@ export class AppointmentComponent implements OnInit {
   bookedTime:string;
   resourceName: string;
   options: any;
+  selectedService:any;
   eventsModel: any;
   showModal: boolean;  
   @ViewChild(FullCalendarComponent, {static: true}) calendarComponent: FullCalendarComponent;
@@ -86,6 +97,14 @@ export class AppointmentComponent implements OnInit {
   calendarPlugins = [dayGridPlugin, timeGrigPlugin, interactionPlugin, resourceTimeGridPlugin];
   calendarWeekends = true;
   todayDate = new Date();
+  services : Service = {
+    name :""
+  };
+  serviceList: Service[] = [
+    {name: 'Hair Cut'},
+    {name: 'Bleech'},
+    {name: 'Facial'}
+  ];
   resources: any[] = [
     {id:'1',title:'Devi'},
     {id:'2',title:'Ramya'},
@@ -98,6 +117,8 @@ export class AppointmentComponent implements OnInit {
     {id:'9',title:'Ashika'},
     {id:'10',title:'Ravi'}
   ];  
+  displayedServiceColumns: string[] = ['name'];
+  dataServiceSource = new MatTableDataSource(SERVICE_DATA);
   status: Status[] = [
     {value: 'New', viewValue: 'New'},
     {value: 'Arrived', viewValue: 'Customer Arrived'},
@@ -124,26 +145,32 @@ export class AppointmentComponent implements OnInit {
   //   { title: 'Hair Coloring',  customerName:'Nisha', customerMobile:'9632574512', customerEmail:'nisha23@gmail.com', resourceId:'3', startTime: new Date('2019-10-16'), staffName:'Nithya',  source:'Walk-in', bookedTime:'04:20PM', backgroundColor:'#34cf7d' }
   // ]; 
   events: any = [
-    {id:'1', title: 'Hair Cut', resourceId:'1', start: new Date(),  backgroundColor:'#8fc8ef'},
-    {id:'2',  title: 'Bleech',  resourceId:'2',start: new Date(), backgroundColor:'#fbf0a2' },
-    {id:'3',  title: 'Facial',  resourceId:'1', start: new Date('2019-10-17'),backgroundColor:'#fde6bc' },
-    {id:'4',  title: 'Hair Coloring', resourceId:'3', start: new Date('2019-10-16'),  backgroundColor:'#79f5b3' },
-    {id:'5',  title: 'Tan Mask', resourceId:'3', start: new Date(),  backgroundColor:'#ecaaaa' }
+    {id:'1', title: 'Hair Cut', resourceId:'1', start: new Date('2019-11-20 02:30:00'),  backgroundColor:'#F3565D'},
+    {id:'2',  title: 'Bleech',  resourceId:'2',start: new Date('2019-11-18 05:30:00'), backgroundColor:'##FF5722' },
+    {id:'3',  title: 'Facial',  resourceId:'1', start: new Date('2019-11-17 10:15:00'),backgroundColor:'#f14343' },
+    {id:'4',  title: 'Hair Coloring', resourceId:'3', start: new Date('2019-11-16 03:00:00'),  backgroundColor:'#9B59B6' },
+    {id:'5',  title: 'Tan Mask', resourceId:'3', start: new Date(),  backgroundColor:'#4d31e6' },
+    {id:'6',  title: 'Body Polishing',  resourceId:'1', start: new Date('2019-11-24 11:15:00'),backgroundColor:'#1BBC9B' },
+    {id:'7',  title: 'Threading',  resourceId:'1', start: new Date('2019-11-28 09:35:00'),backgroundColor:'#43bc1b' },
+    {id:'8',  title: 'Body Polishing',  resourceId:'1', start: new Date('2019-11-04 12:20:00'),backgroundColor:'#6d6d6d' },
+    {id:'9',  title: 'Threading',  resourceId:'1', start: new Date('2019-11-08 06:00:00'),backgroundColor:'#6d6d6d' },
+    {id:'10',  title: 'Body Polishing',  resourceId:'1', start: new Date('2019-10-24 04:10:00'),backgroundColor:'#6d6d6d' },
+    {id:'11',  title: 'Threading',  resourceId:'1', start: new Date('2019-09-05 10:30:00'),backgroundColor:'#6d6d6d' },
   ]; 
   eventClick(model:any) {
     this.serviceTitle = model.event.title;
     this.startTime = model.event.start;   
     this.eventId = model.event.id;
     this.bookingStatusBg = model.event.backgroundColor;    
-    if(this.bookingStatusBg == '#8fc8ef') {
+    if(this.bookingStatusBg == '#F3565D') {
       this.bookingStatus = 'New';
-    } else if(this.bookingStatusBg == '#fbf0a2') {
+    } else if(this.bookingStatusBg == '#FF5722') {
       this.bookingStatus = 'Arrived';
     }
-    else if(this.bookingStatusBg == '#fde6bc') {
+    else if(this.bookingStatusBg == '#1BBC9B') {
       this.bookingStatus = 'Started';
     }
-    else if(this.bookingStatusBg == '#79f5b3') {
+    else if(this.bookingStatusBg == '#9B59B6') {
       this.bookingStatus = 'Completed';
     } else {
       this.bookingStatus = 'Cancelled';
@@ -175,24 +202,12 @@ export class AppointmentComponent implements OnInit {
   startBill(billId) {
     this.router.navigate(['/beautelabs/cbot-admin/saleCreate', billId])
   }
-  updateHeader() {
-    this.options.header = {
-      left: 'prev,next myCustomButton',
-      center: 'title',
-      right: ''
-    };
-  }
-  updateEvents() {
-    this.eventsModel = [{
-      title: 'Updaten Event',
-      start: this.yearMonth + '-08',
-      end: this.yearMonth + '-10'
-    }];
-  }
+  
   get yearMonth(): string {
     const dateObj = new Date();
     return dateObj.getUTCFullYear() + '-' + (dateObj.getUTCMonth() + 1);
   }
+  
   handleDateClick(arg) {
     if(arg.date >= this.todayDate) {    
       this.router.navigate(['/beautelabs/cbot-admin/bookingCreate', { Seldate: arg.dateStr }]);
@@ -222,15 +237,67 @@ export class AppointmentComponent implements OnInit {
     this.calString = "Calendar";
     this.selIcon = "event";    
     this.options = {
-      editable: true,     
+      editable: true,
+      eventLimit: true, // for all non-TimeGrid views
       header: {
-        left: 'prev,next today myCustomButton',
+        left: 'prev,next today',
         center: 'title',
-        right: 'dayGridMonth,timeGridWeek,timeGridDay'
+        right: 'timeGridDay,timeGridWeek,dayGridMonth,listWeek'
       },    
       // add other plugins
-      plugins: [dayGridPlugin, interactionPlugin, timeGridPlugin, resourceTimeGridPlugin]
-    };
+      plugins: [dayGridPlugin, interactionPlugin, timeGridPlugin, resourceTimeGridPlugin, listPlugin],    
+      rerenderEvents: function() {
+        console.log('tst');
+        
+      },
+      views: {
+        timeGrid: {
+          eventLimit: 6 // adjust to 6 only for timeGridWeek/timeGridDay
+        },
+        dayGrid: {
+          eventLimit: 4
+        }
+      },     
+      eventRender: function(event, eventElement) {
+        if (event) {
+            eventElement.find("div.fc-content").prepend("<span class='fc-event-dot'></span>");
+        }
+    },
+      eventAfterRender: function (event, element) {
+        (element).tooltip({title:event.title, container: "body"});
+      }
+    // eventAfterAllRender: function (view) {
+    //   // Count events
+    //     var quantity = ('.fc-event').length;
+    //     console.log(quantity);
+    //     return quantity;        
+        
+    // },
+      
+//       dayClick: function(date, allDay, jsEvent, view) {
+//         if (!allDay) {
+//           // strip time information
+//           date = new Date(date.getFullYear(), date.getMonth(), date.getDay());
+//         }
+//         var ar =  function(event) {
+//           if (event.start.year() == date.year() && event.start.month() == date.month() && event.start.day() == date.day()) {
+//             return true;
+//           }
+//           return false;
+//         };
+//         console.log(ar.length);
+//       },
+//       eventRender: function( event, element, view,info ) {
+//         element.find('.fc-title').prepend('<span>123</span> '); 
+//         var tooltip = new tooltip(info.el, {
+//           title: info.event.extendedProps.description,
+//           placement: 'top',
+//           trigger: 'hover',
+//           container: 'body'
+//         });
+//    }
+     };
+     
     
 }
 
@@ -256,4 +323,23 @@ caltoggle() {
     this.calicon = "event_note";
   }
 }
+
+addNew(){
+  SERVICE_DATA.push(this.services)
+  this.dataServiceSource = new MatTableDataSource(SERVICE_DATA);
+  this.services = {
+    name :""
+  };
+  }
+  selectChangeHandler (event: any) {    
+    this.selectedService = event.target.value;
+  }
+  eventRender(event) {
+ //  console.log(event);
+   return event.el.innerText;
+  }   
+  dayRender(e) {    
+    console.log(e);
+  }
+
 }
